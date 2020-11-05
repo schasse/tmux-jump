5893e176056fe3286266a08c78cb776962444102
Roy O'Young <roy2220@outlook.com>
2020-11-05 23:21:04 +0800

    Fix #29

--- scripts/tmux-jump.rb
+++ scripts/tmux-jump.rb
@@ -12,6 +12,7 @@ HOME_SEQ = "\e[H"
 RESET_COLORS = "\e[0m"
 ENTER_ALTERNATE_SCREEN = "\e[?1049h"
 RESTORE_NORMAL_SCREEN = "\e[?1049l"
+NEWLINE = "\r\n"
 
 # CONFIG
 KEYS = 'jfhgkdlsa'.each_char.to_a
@@ -23,6 +24,7 @@ Config = Struct.new(
   :cursor_x,
   :alternate_on,
   :scroll_position,
+  :pane_width,
   :pane_height,
   :tmp_file
 ).new
@@ -60,7 +62,7 @@ end
 def recover_alternate_screen_after
   saved_screen =
     `tmux capture-pane -ep -t #{Config.pane_nr}`[0..-2] # with colors...
-      .gsub("\n", "\n\r")
+      .gsub("\n", NEWLINE)
   File.open(Config.pane_tty_file, 'a') do |tty|
     tty << CLEAR_SEQ + HOME_SEQ
   end
@@ -130,28 +132,51 @@ def async_detect_user_escape(result_queue)
   end
 end
 
-def positions_of(jump_to_char, screen_chars)
-  positions = []
+def make_screen_lines(screen_chars)
+  screen_lines = screen_chars.split("\n")
+  screen_lines.each_with_index do |screen_line, i|
+    screen_lines[i] = screen_line + ' ' * (Config.pane_width - screen_line.size)
+  end
+  screen_lines += [' ' * Config.pane_width] * (Config.pane_height - screen_lines.size)
+  screen_lines
+end
 
-  positions << 0 if screen_chars[0] =~ /\w/ && screen_chars[0].downcase == jump_to_char
-  screen_chars.each_char.with_index do |char, i|
-    if (char =~ /\w/).nil? && screen_chars[i+1] && screen_chars[i+1].downcase == jump_to_char
-      positions << i+1
+def positions_of(jump_to_char, screen_lines)
+  positions = []
+  offset = 0
+  screen_lines.each_with_index do |screen_line, line_idx|
+    prev_char = ''
+    screen_line.each_char.each_with_index do |char, column_idx|
+      positions << [line_idx, column_idx, offset] if !(prev_char =~ /\w/) && char.downcase == jump_to_char
+      offset += 1
+      prev_char = char
     end
+    offset += NEWLINE.size
   end
   positions
 end
 
-def draw_keys_onto_tty(screen_chars, positions, keys, key_len)
-  File.open(Config.pane_tty_file, 'a') do |tty|
-    cursor = 0
-    positions.each_with_index do |pos, i|
-      tty << "#{GRAY}#{screen_chars[cursor..pos-1].gsub("\n", "\n\r")}"
-      tty << "#{RED}#{keys[i]}"
-      cursor = pos + key_len
+def draw_keys_onto_tty(screen_lines, positions, keys, key_len)
+  screen = screen_lines.join(NEWLINE)
+  cursor = 0
+  segments = []
+  positions.each_with_index do |pos, i|
+    offset = pos[2]
+    if offset > cursor
+      segments << GRAY
+      segments << screen[cursor..offset-1]
     end
-    tty << "#{GRAY}#{screen_chars[cursor..-1].gsub("\n", "\n\r")}"
-    tty << HOME_SEQ
+    segments << RED
+    segments << keys[i]
+    cursor = offset + key_len
+  end
+  if screen.size > cursor
+    segments << GRAY
+    segments << screen[cursor..-1]
+  end
+  screen_with_keys = segments.join()
+  File.open(Config.pane_tty_file, 'a') do |tty|
+    tty << screen_with_keys + HOME_SEQ
   end
 end
 
@@ -163,12 +188,12 @@ def keys_for(position_count, keys = KEYS)
   end
 end
 
-def prompt_position_index!(positions, screen_chars) # raises Timeout::Error
+def prompt_position_index!(positions, screen_lines) # raises Timeout::Error
   return nil if positions.size == 0
   return 0 if positions.size == 1
   keys = keys_for positions.size
   key_len = keys.first.size
-  draw_keys_onto_tty screen_chars, positions, keys, key_len
+  draw_keys_onto_tty screen_lines, positions, keys, key_len
   key_index = KEYS.index(prompt_char!)
   if !key_index.nil? && key_len > 1
     magnitude = KEYS.size ** (key_len - 1)
@@ -176,7 +201,7 @@ def prompt_position_index!(positions, screen_chars) # raises Timeout::Error
     range_ending = range_beginning + magnitude - 1
     remaining_positions = positions[range_beginning..range_ending]
     return nil if remaining_positions.nil?
-    lower_index = prompt_position_index!(remaining_positions, screen_chars)
+    lower_index = prompt_position_index!(remaining_positions, screen_lines)
     return nil if lower_index.nil?
     range_beginning + lower_index
   else
@@ -195,28 +220,25 @@ def main
   ending = -Config.scroll_position + Config.pane_height - 1
   screen_chars =
     `tmux capture-pane -p -t #{Config.pane_nr} -S #{start} -E #{ending}`[0..-2].gsub("︎", '') # without colors
-  positions = positions_of jump_to_char, screen_chars
+  screen_lines = make_screen_lines screen_chars
+  positions = positions_of jump_to_char, screen_lines
   position_index = recover_screen_after do
-    prompt_position_index! positions, screen_chars
+    prompt_position_index! positions, screen_lines
   end
   Kernel.exit 0 if position_index.nil?
   jump_to = positions[position_index]
+  Kernel.exit 0 if jump_to.nil?
   `tmux copy-mode -t #{Config.pane_nr}`
-   # begin: tmux weirdness when 1st line is empty
-  `tmux send-keys -X -t #{Config.pane_nr} start-of-line`
   `tmux send-keys -X -t #{Config.pane_nr} top-line`
-  `tmux send-keys -X -t #{Config.pane_nr} -N 200 cursor-right`
-   # end
+  `tmux send-keys -X -t #{Config.pane_nr} -N #{jump_to[0]} cursor-down` if jump_to[0] >= 1
   `tmux send-keys -X -t #{Config.pane_nr} start-of-line`
-  `tmux send-keys -X -t #{Config.pane_nr} top-line`
-  `tmux send-keys -X -t #{Config.pane_nr} -N #{Config.scroll_position} cursor-up`
-  `tmux send-keys -X -t #{Config.pane_nr} -N #{jump_to} cursor-right`
+  `tmux send-keys -X -t #{Config.pane_nr} -N #{jump_to[1]} cursor-right` if jump_to[1] >= 1
 end
 
 if $PROGRAM_NAME == __FILE__
   Config.pane_nr = `tmux display-message -p "\#{pane_id}"`.strip
   format = '#{pane_id};#{pane_tty};#{pane_in_mode};#{cursor_y};#{cursor_x};'\
-           '#{alternate_on};#{scroll_position};#{pane_height}'
+           '#{alternate_on};#{scroll_position};#{pane_width};#{pane_height}'
   tmux_data = `tmux display-message -p -t #{Config.pane_nr} -F "#{format}"`.strip.split(';')
   Config.pane_tty_file = tmux_data[1]
   Config.pane_mode = tmux_data[2]
@@ -224,7 +246,8 @@ if $PROGRAM_NAME == __FILE__
   Config.cursor_x = tmux_data[4]
   Config.alternate_on = tmux_data[5]
   Config.scroll_position = tmux_data[6].to_i
-  Config.pane_height = tmux_data[7].to_i
+  Config.pane_width = tmux_data[7].to_i
+  Config.pane_height = tmux_data[8].to_i
   Config.tmp_file = ARGV[0]
   main
 end
